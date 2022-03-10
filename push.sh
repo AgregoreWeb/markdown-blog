437c3d933170f70de809b98debeb70758eb4325a
Dirk Uys <dirkcuys@gmail.com>
2022-03-10 21:25:59 +0200

    update script to push to node + testing js-ipfs-fetch

--- push.sh
+++ push.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-IPFS_NODE="http://192.168.1.60:5001"
+IPFS_NODE="http://localhost:5001"
+
+echo Creating folder
+curl -X POST "$IPFS_NODE/api/v0/files/mkdir?arg=/ipmb"
+
 for f in `ls`
 do
     echo Adding $f
     curl -X POST -F file=@$f "$IPFS_NODE/api/v0/files/write?arg=/ipmb/$f&create=true"        
 done
-FOLDER_HASH=`curl -s -X POST "http://192.168.1.60:5001/api/v0/files/stat?arg=/ipmb" | jq -r .Hash`
+FOLDER_HASH=`curl -s -X POST "$IPFS_NODE/api/v0/files/stat?arg=/ipmb" | jq -r .Hash`
 
 echo Publishing /ipmb with key: $FOLDER_HASH
-IPNS_NAME = `curl -s -X POST "$IPFS_NODE/api/v0/name/publish?arg=$FOLDER_HASH" | jq -r .Name`
+IPNS_NAME=`curl -s -X POST "$IPFS_NODE/api/v0/name/publish?arg=$FOLDER_HASH" | jq -r .Name`
 
 echo View site at ipns://$IPNS_NAME
