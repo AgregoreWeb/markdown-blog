ee212a8da5d02dd9fbb5fd3d28b77340ff6bdaff
Dirk Uys <dirkcuys@gmail.com>
2022-04-13 15:14:43 +0200

    make sure filename has date and ends with .md

--- create-post.js
+++ create-post.js
@@ -29,6 +29,13 @@ export default class CreatePost extends HTMLElement {
     this._shadow.querySelector('form').addEventListener('submit', e => {
       e.preventDefault();
       let filename = this._shadow.querySelector('input[name="filename"]').value;
+      if (! filename.match(/\d\d\d\d-\d\d-\d\d-/)){
+        let date = new Date().toISOString().slice(0, 10); //YYYY-mm-dd
+        filename = `${date}-${filename}`;
+      }
+      if (! filename.match(/\.md$/) ){
+        filename = `${filename}.md`;
+      }
       let content = this._shadow.querySelector('textarea[name="content"]').value;
       console.log(`CreatePost.connectedCallback.submit ${filename} ${content}`);
       this.dispatchEvent(new CustomEvent('onSubmit', { detail: { filename, content } }));
